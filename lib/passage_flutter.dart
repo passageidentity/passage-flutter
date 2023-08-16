a59f95329337e4f8fb492baf4d52478dd076c64b
Ricky Padilla <rickycpadilla@gmail.com>
2023-08-16 16:50:13 -0600

    feat(PSG-2624): added inline docs

--- lib/passage_flutter.dart
+++ lib/passage_flutter.dart
@@ -7,98 +7,307 @@ import 'passage_flutter_platform/passage_flutter_platform_interface.dart';
 class PassageFlutter {
   // PASSKEY AUTH METHODS
 
+  /// Attempts to create and register a new user with a passkey.
+  ///
+  /// The method will try to register a user using the provided identifier which
+  /// can be either an email address or a phone number.
+  ///
+  /// Parameters:
+  ///  - `identifier`: Email address or phone for the user.
+  ///
+  /// Returns:
+  ///  A `Future<AuthResult>` object that includes a redirect URL and saves the
+  ///  authorization token and (optional) refresh token securely to the device.
+  ///
+  /// Throws:
+  ///  A `PassageError` in cases such as:
+  ///  - User cancels the operation
+  ///  - User already exists
+  ///  - App configuration was not done properly
+  ///  - etc.
   Future<AuthResult> register(String identifier) {
     return PassageFlutterPlatform.instance.register(identifier);
   }
 
+  /// Attempts to login a user with a passkey.
+  ///
+  /// **NOTE:** Web apps should use `loginWithIdentifier` instead.
+  ///
+  /// Returns:
+  ///  A `Future<AuthResult>` object that includes a redirect URL and saves the
+  ///  authorization token and (optional) refresh token securely to the device.
+  ///
+  /// Throws:
+  ///  A `PassageError` in cases such as:
+  ///  - User cancels the operation
+  ///  - User does not exist
+  ///  - App configuration was not done properly
+  ///  - etc.
   Future<AuthResult> login() {
     return PassageFlutterPlatform.instance.login();
   }
 
+  /// Attempts to login a user with a passkey.
+  ///
+  /// **NOTE:** Mobile apps should use `login` instead.
+  ///
+  /// Returns:
+  ///  A `Future<AuthResult>` object that includes a redirect URL and saves the
+  ///  authorization token and (optional) refresh token securely to the device.
+  ///
+  /// Throws:
+  ///  A `PassageError` in cases such as:
+  ///  - User cancels the operation
+  ///  - User does not exist
+  ///  - App configuration was not done properly
+  ///  - etc.
   Future<AuthResult> loginWithIdentifier(String identifier) {
     return PassageFlutterPlatform.instance.loginWithIdentifier(identifier);
   }
 
   // OTP METHODS
 
+  /// Creates and sends a new one-time passcode for registration.
+  ///
+  /// Parameters:
+  ///  - `identifier`: The Passage User's identifier.
+  ///
+  /// Returns:
+  ///  A `Future<String>` that returns a one-time passcode ID used to activate
+  ///  the passcode in `oneTimePasscodeActivate`.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<String> newRegisterOneTimePasscode(String identifier) {
     return PassageFlutterPlatform.instance
         .newRegisterOneTimePasscode(identifier);
   }
 
+  /// Creates and sends a new one-time passcode for logging in.
+  ///
+  /// Parameters:
+  ///  - `identifier`: The Passage User's identifier.
+  ///
+  /// Returns:
+  ///  A `Future<String>` that returns a one-time passcode ID used to activate
+  ///  the passcode in `oneTimePasscodeActivate`.
+  ///
+  /// Throws:
+  ///  `PassageError`
+
   Future<String> newLoginOneTimePasscode(String identifier) {
     return PassageFlutterPlatform.instance.newLoginOneTimePasscode(identifier);
   }
 
+  /// Activates a one-time passcode when a user inputs it. This function handles both login and registration one-time passcodes.
+  ///
+  /// Parameters:
+  ///  - `otp`: The one-time passcode.
+  ///  - `otpId`: The one-time passcode ID.
+  ///
+  /// Returns:
+  ///  A `Future<AuthResult>` object that includes a redirect URL and saves the
+  ///  authorization token and (optional) refresh token securely to the device.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<AuthResult> oneTimePasscodeActivate(String otp, String otpId) {
     return PassageFlutterPlatform.instance.oneTimePasscodeActivate(otp, otpId);
   }
 
   // MAGIC LINK METHODS
 
+  /// Creates and sends a new magic link for registration.
+  ///
+  /// Parameters:
+  ///  - `identifier`: The Passage User's identifier.
+  ///
+  /// Returns:
+  ///  A `Future<String>` that returns a magic link ID used to check the status
+  ///  of the magic link with `getMagicLinkStatus`.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<String> newRegisterMagicLink(String identifier) {
     return PassageFlutterPlatform.instance.newRegisterMagicLink(identifier);
   }
 
+  /// Creates and sends a new magic link for logging in.
+  ///
+  /// Parameters:
+  ///  - `identifier`: The Passage User's identifier.
+  ///
+  /// Returns:
+  ///  A `Future<String>` that returns a magic link ID used to check the status
+  ///  of the magic link with `getMagicLinkStatus`.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<String> newLoginMagicLink(String identifier) {
     return PassageFlutterPlatform.instance.newLoginMagicLink(identifier);
   }
 
+  /// Activates a magic link. This function handles both login and registration magic links.
+  ///
+  /// Parameters:
+  ///  - `magicLink`: The magic link from the URL sent to the user.
+  ///
+  /// Returns:
+  ///  A `Future<AuthResult>` object that includes a redirect URL and saves the
+  ///  authorization token and (optional) refresh token securely to the device.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<AuthResult> magicLinkActivate(String magicLink) {
     return PassageFlutterPlatform.instance.magicLinkActivate(magicLink);
   }
 
+  /// Looks up a magic link by ID and checks if it has been verified. This function is most commonly used to
+  /// iteratively check if a user has clicked a magic link to login. Once the link has been verified,
+  /// Passage will return authentication information via this function. This enables cross-device login.
+  ///
+  /// Parameters:
+  ///  - `magicLinkId`: The magic link ID.
+  ///
+  /// Returns:
+  ///  A `Future<AuthResult?>` object that includes a redirect URL and saves the
+  ///  authorization token and (optional) refresh token securely to the device, or `null` if
+  ///  the magic link has not been verified.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<AuthResult?> getMagicLinkStatus(String magicLinkId) {
     return PassageFlutterPlatform.instance.getMagicLinkStatus(magicLinkId);
   }
 
   // TOKEN METHODS
 
+  /// Returns the auth token for the currently authenticated user.
+  ///
+  /// Returns:
+  ///  A `Future<String?>` representing the current Passage user's auth token,
+  ///  or `null` if no token has been stored.
   Future<String?> getAuthToken() {
     return PassageFlutterPlatform.instance.getAuthToken();
   }
 
+  /// Checks if the auth token for the currently authenticated user is valid.
+  ///
+  /// Returns:
+  ///  A `Future<bool>` that returns `true` if the user has a valid auth token,
+  ///  and `false` otherwise.
   Future<bool> isAuthTokenValid(String authToken) {
     return PassageFlutterPlatform.instance.isAuthTokenValid(authToken);
   }
 
+  /// Refreshes, retrieves, and saves a new authToken for the currently authenticated user using their refresh token.
+  ///
+  /// Returns:
+  ///  A `Future<String>` representing the new auth token.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<String> refreshAuthToken() {
     return PassageFlutterPlatform.instance.refreshAuthToken();
   }
 
+  /// Sign out a user by deleting their auth token and refresh token from device, and revoking their refresh token.
   Future<void> signOut() {
     return PassageFlutterPlatform.instance.signOut();
   }
 
   // APP METHODS
 
+  /// Gets information about an app.
+  ///
+  /// Returns:
+  ///  A `Future<PassageAppInfo>` object containing app information, authentication policy, etc.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<PassageAppInfo?> getAppInfo() {
     return PassageFlutterPlatform.instance.getAppInfo();
   }
 
   // USER METHODS
 
+  /// Returns the user information for the currently authenticated user.
+  ///
+  /// Returns:
+  ///  A `Future<PassageUser?>` representing the current Passage user's info,
+  ///  or `null` if the current Passage user's authentication token could not be validated.
   Future<PassageUser?> getCurrentUser() {
     return PassageFlutterPlatform.instance.getCurrentUser();
   }
 
+  /// Attempts to create and register a new passkey for the authenticated user.
+  ///
+  /// Returns:
+  ///  A `Future<Passkey>` representing an object containing all of the data about the new passkey.
+  ///
+  /// Throws:
+  ///  A `PassageError` in cases such as:
+  ///  - User cancels the operation
+  ///  - App configuration was not done properly
+  ///  - etc.
   Future<Passkey> addPasskey() {
     return PassageFlutterPlatform.instance.addPasskey();
   }
 
+  /// Removes a passkey from a user's account.
+  /// NOTE: This does NOT remove the passkey from the user's device, but revokes that passkey so it's no longer usable.
+  ///
+  /// Parameters:
+  ///  - `passkeyId`: The ID of the passkey to delete.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<void> deletePasskey(String passkeyId) {
     return PassageFlutterPlatform.instance.deletePasskey(passkeyId);
   }
 
+  /// Edits the `friendlyName` of the authenticated user's device passkey.
+  ///
+  /// Parameters:
+  ///  - `passkeyId`: The ID of the passkey to edit.
+  ///  - `newPasskeyName`: The new name for the passkey.
+  ///
+  /// Returns:
+  ///  A `Future<Passkey>` object containing all of the data about the updated passkey.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<Passkey> editPasskeyName(String passkeyId, String newPasskeyName) {
     return PassageFlutterPlatform.instance
         .editPasskeyName(passkeyId, newPasskeyName);
   }
 
+  /// Initiates an email change for the authenticated user. An email change requires verification,
+  /// so an email will be sent to the user which they must verify before the email change takes effect.
+  ///
+  /// Parameters:
+  ///  - `newEmail`: The user's new email.
+  ///
+  /// Returns:
+  ///  A `Future<String>` representing the magic link ID.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<String> changeEmail(String newEmail) {
     return PassageFlutterPlatform.instance.changeEmail(newEmail);
   }
 
+  /// Initiates a phone number change for the authenticated user. A phone change requires verification,
+  /// so an email will be sent to the user which they must verify before the phone change takes effect.
+  ///
+  /// Parameters:
+  ///  - `newPhone`: The user's new phone number.
+  ///
+  /// Returns:
+  ///  A `Future<String>` representing the magic link ID.
+  ///
+  /// Throws:
+  ///  `PassageError`
   Future<String> changePhone(String newPhone) {
     return PassageFlutterPlatform.instance.changePhone(newPhone);
   }
