47c5be5bc45a1456bb9fbd1b78e3238de3c10bf4
Ricky Padilla <rickycpadilla@gmail.com>
2023-08-21 15:26:12 -0600

    chore: made error codes accessible to all platforms

---
+++ lib/passage_flutter_models/passage_error_code.dart
@@ -0,0 +1,14 @@
+class PassageErrorCode {
+  static const passkeysNotSupported = 'PASSKEYS_NOT_SUPPORTED';
+  static const invalidArgument = 'INVALID_ARGUMENT';
+  static const passkeyError = 'PASSKEY_ERROR';
+  static const userCancelled = 'USER_CANCELLED';
+  static const userUnauthorized = 'USER_UNAUTHORIZED';
+  static const otpError = 'OTP_ERROR';
+  static const magicLinkError = 'MAGIC_LINK_ERROR';
+  static const tokenError = 'TOKEN_ERROR';
+  static const appInfoError = 'APP_INFO_ERROR';
+  static const changeEmailError = 'CHANGE_EMAIL_ERROR';
+  static const changePhoneError = 'CHANGE_PHONE_ERROR';
+  static const unknkown = 'UNKNOWN';
+}
