f7313a8467b295796079689f6fa8a08ac2c69170
Sina <65910646+SinaSeylani@users.noreply.github.com>
2024-06-05 13:15:35 -0600

    Expiry date updated to 2028

--- test/auth_token_test.dart
+++ test/auth_token_test.dart
@@ -1,15 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:passage_flutter/passage_flutter_web.dart';
 
-
-
 void main() {
   group('AuthToken validation', () {
     final passageFlutterWeb = PassageFlutterWeb();
 
     test('Valid token should return true', () async {
-      // Token with an expiration time in the future (e.g., Dec 31, 2024)
-      const validToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzU2ODk2MDB9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+      // Token with an expiration time in the future (e.g., Dec 31, 2028)
+      const validToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE4MzAyOTc2MDB9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
       final result = await passageFlutterWeb.isAuthTokenValid(validToken);
       expect(result, true);
     });
