8a7475358dcd080a20f1867326523c098f280ff2
Sina <65910646+SinaSeylani@users.noreply.github.com>
2024-06-06 14:11:40 -0600

    Flutter ci added

--- android/src/main/kotlin/id/passage/passage_flutter/PassageFlutterPlugin.kt
+++ android/src/main/kotlin/id/passage/passage_flutter/PassageFlutterPlugin.kt
@@ -43,6 +43,7 @@ class PassageFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
       }
 
       when (call.method) {
+          "initWithAppId" -> {}
           "registerWithPasskey" -> passageFlutter?.registerWithPasskey(call, result)
           "loginWithPasskey" -> passageFlutter?.loginWithPasskey(call, result)
           "deviceSupportsPasskeys" -> passageFlutter?.deviceSupportsPasskeys(result)
