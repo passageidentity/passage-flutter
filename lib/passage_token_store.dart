1e3761354b5760ec6aa75f31a9b913548aaa7184
Sina <65910646+SinaSeylani@users.noreply.github.com>
2024-09-09 17:03:37 -0600

    token store class refactored

---
+++ lib/passage_token_store.dart
@@ -0,0 +1,36 @@
+import 'package:passage_flutter/passage_flutter_models/auth_result.dart';
+
+import 'passage_flutter_platform/passage_flutter_platform_interface.dart';
+
+class PassageTokenStore {
+
+  /// Returns the auth token for the currently authenticated user.
+  ///
+  /// Returns:
+  ///  A `Future<String?>` representing the current Passage user's auth token,
+  ///  or `null` if no token has been stored.
+  Future<String> getValidAuthToken() {
+    return PassageFlutterPlatform.instance.getAuthToken();
+  }
+
+  /// Checks if the auth token for the currently authenticated user is valid.
+  ///
+  /// Returns:
+  ///  A `Future<bool>` that returns `true` if the user has a valid auth token,
+  ///  and `false` otherwise.
+  Future<bool> isAuthTokenValid(String authToken) {
+    return PassageFlutterPlatform.instance.isAuthTokenValid(authToken);
+  }
+
+  /// Refreshes, retrieves, and saves a new authToken for the currently authenticated user using their refresh token.
+  ///
+  /// Returns:
+  ///  A `Future<String>` representing the new auth token.
+  ///
+  /// Throws:
+  ///  `PassageError`
+  Future<AuthResult> refreshAuthToken() {
+    return PassageFlutterPlatform.instance.refreshAuthToken();
+  }
+
+}
